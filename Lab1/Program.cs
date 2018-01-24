e9ce82ea5f10eb9c8d5363ac4a338aa07ac29ea7
John <jjfloreno@gmail.com>
2018-01-24 11:12:45 -0500

    added methods for calculations, improved valdiation for user to continue

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -12,7 +12,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             //input
-            double Length, Width, Height, Area, Volume, Perimeter;
+            double Length, Width, Height;
             char userContinue;
             bool doAgain = true;
 
@@ -27,48 +27,66 @@ namespace Lab1
                 Console.WriteLine("Please enter the room's height");
                 Height = double.Parse(Console.ReadLine());
 
-                //processing
-                Area = Length * Width;
-                Volume = Length * Width * Height;
-                Perimeter = Length * 2 + Width * 2;
-
                 //output
                 Console.WriteLine();
-                Console.WriteLine($"Area: {Area} sq ft");
-                Console.WriteLine($"Volume: {Volume} cu ft");
-                Console.WriteLine($"Perimeter: {Perimeter} ft");
+                Console.WriteLine($"Area: " + GetArea(Length, Width) + " sq ft");
+                Console.WriteLine($"Volume: " + GetVol(Length, Width, Height) + " cu ft");
+                Console.WriteLine($"Perimeter: " + GetPerimeter(Length, Width) + " ft");
                 Console.WriteLine();
 
                 Console.WriteLine("Would you like to continue? (Y/N)");
 
                 //input -- to run program again or not
-                userContinue = char.Parse(Console.ReadLine());
-
-                switch (userContinue)
+                if (char.TryParse(Console.ReadLine(), out userContinue))
                 {
-                    case 'n':
-                        Console.WriteLine("Goodbye!");
-                        System.Threading.Thread.Sleep(1000);
-                        doAgain = false;
-                        break;
-                    case 'N':
-                        Console.WriteLine("Goodbye!");
-                        System.Threading.Thread.Sleep(1000);
-                        doAgain = false;
-                        break;
-                    case 'y':
-                        Console.WriteLine("Ok, here we go!");
-                        System.Threading.Thread.Sleep(1000);
-                        Console.WriteLine();
-                        break;
-                    case 'Y':
-                        Console.WriteLine("Ok, here we go!");
-                        System.Threading.Thread.Sleep(1000);
-                        Console.WriteLine();
-                        break;
+                    switch (userContinue)
+                    {
+                        case 'n':
+                            Console.WriteLine("Goodbye!");
+                            System.Threading.Thread.Sleep(1000);
+                            doAgain = false;
+                            break;
+                        case 'N':
+                            Console.WriteLine("Goodbye!");
+                            System.Threading.Thread.Sleep(1000);
+                            doAgain = false;
+                            break;
+                        case 'y':
+                            Console.WriteLine("Ok, here we go!");
+                            System.Threading.Thread.Sleep(1000);
+                            Console.Clear();
+                            break;
+                        case 'Y':
+                            Console.WriteLine("Ok, here we go!");
+                            System.Threading.Thread.Sleep(1000);
+                            Console.Clear();
+                            break;
+                    }
                 }
-            }
+                else //will quit with an invalid userInput
+                {
+                    Console.WriteLine("Not a valid input. Goodbye!");
+                    Console.Read();
+                    doAgain = false;
+                }
 
+
+            }
+        }    
+        //method to get area -- return double, takes 2 doubles (L & W)
+        public static double GetArea(double Length, double Width)
+        {
+            return Length * Width;
+        }
+        //method to get volume -- return double, takes 3 dbls (L & W & H)
+        public static double GetVol(double Length, double Width, double Height)
+        {
+            return Length * Width * Height;
+        }
+        //method to get area -- return double, takes 2 dbl (L & W)
+        public static double GetPerimeter(double Length, double Width)
+        {
+            return Length * 2 + Width*2;
         }
     }
 }
